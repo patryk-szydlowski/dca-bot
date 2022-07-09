46d9c81f4dc0ec8458a2261c12daec227688cdfd
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-09 20:27:14 +0200

    chore: fix Node resolution

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { SystemLogger } from "./logging/adapter/system";
 import { createTradingService, TradingProvider } from "./trading/adapter/factory";
 import { checkBalance } from "./trading/use-cases/check-balance";
 
-const logger = new SystemLogger();
-const tradingService = await createTradingService({ provider: TradingProvider.XTB });
+const program = async () => {
+  const logger = new SystemLogger();
+  const tradingService = await createTradingService({ provider: TradingProvider.XTB });
 
-await checkBalance({ logger, tradingService });
+  await checkBalance({ logger, tradingService });
+};
+
+void program();
